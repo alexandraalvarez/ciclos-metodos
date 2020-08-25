45f3ecc575d124f0ff13229d6bba99330b5eea04
alexandraalvarez <alexandra.alvarez.a@gmail.com>
2020-08-25 18:06:25 -0400

    tareas 1 y 2, listas, tarea 3 en proceso

---
+++ assets/tareas.js
@@ -0,0 +1,34 @@
+function init(){
+}
+    
+function myFunction() {
+    var number = parseInt(prompt("Ingrese un numero del 1 al 6"));
+    
+    switch(number) {
+        case 1:
+            var caso1 = prompt("Ingrese un número del 1 al 100");
+            for (let i = 0; i <= caso1; i++) {
+                //alert(i);
+                console.log(i);
+            };
+            break;
+        case 2:
+            do {
+                var caso2str = prompt("¿De qué color es el caballo blanco de Napoleón?");
+                alert(caso2str);
+                var caso2 = caso2str.toLowerCase();
+                alert(caso2);
+            } while (caso2 !== "blanco");
+            break; 
+        case 3:    
+            var mateMedia = prompt("Ingrese su promedio de matemáticas");
+            var fisimedia = prompt("Ingrese su promedio de física");
+            var cienciaMedia = prompt("Ingrese su promedio de ciencias");
+
+        }
+
+}
+        
+
+
+
